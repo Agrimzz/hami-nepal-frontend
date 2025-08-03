1320809e3a32bd97a9b89f2927db5f8b53e955ed
Agrim Pradhananga <agrimp91@gmail.com>
2025-08-03 11:39:28 +0545

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,46 @@
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "react-native-reanimated";
+import "./global.css";
+SplashScreen.preventAutoHideAsync();
+export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "BricolageGrotesque-Bold": require("../assets/fonts/BricolageGrotesque-Bold.ttf"),
+    "BricolageGrotesque-ExtraBold": require("../assets/fonts/BricolageGrotesque-ExtraBold.ttf"),
+    "BricolageGrotesque-ExtraLight": require("../assets/fonts/BricolageGrotesque-ExtraLight.ttf"),
+    "BricolageGrotesque-Light": require("../assets/fonts/BricolageGrotesque-Light.ttf"),
+    "BricolageGrotesque-Medium": require("../assets/fonts/BricolageGrotesque-Medium.ttf"),
+    "BricolageGrotesque-Regular": require("../assets/fonts/BricolageGrotesque-Regular.ttf"),
+    "BricolageGrotesque-SemiBold": require("../assets/fonts/BricolageGrotesque-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+  return (
+    <GestureHandlerRootView>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="inventory" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="inventory/(general)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="inventory/(noheader)"
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen name="form" options={{ headerShown: false }} />
+      </Stack>
+    </GestureHandlerRootView>
+  );
+}
